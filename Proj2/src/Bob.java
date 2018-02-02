0bb79ed9e11294a8d5cd20024f0c693feafe8146
toapley <toapley@482L072172.lwsd.k12wa.org>
2018-02-01 16:01:09 -0800

    another change

--- Proj2/src/Bob.java
+++ Proj2/src/Bob.java
@@ -1,10 +1,9 @@
 
-public class Main {
+public class Bob {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("This did another change...");
 	}
 
 }
